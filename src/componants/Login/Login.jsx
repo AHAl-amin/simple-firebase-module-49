cfbba6f8fbaf08c135330b0698f43003a1f331cd
Alamin <mdalaminhossain573190@gmail.com>
2024-03-30 16:22:42 +0600

    this is 2nd commit

--- src/componants/Login/Login.jsx
+++ src/componants/Login/Login.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import {GoogleAuthProvider, getAuth} from 'firebase/auth'
+import React, { useState } from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
 import app from '../../firebase/firebase.init';
 
 const Login = () => {
-    const auth =getAuth(app);
+    const [user, setUser] = useState()
+    const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
-    const handleGoogleSignIn =()=>{
-        console.log('google comming')
+
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
+    const handleSignOut =() =>{
+        signOut(auth)
+        .then(result => {
+            console.log(result);
+            setUser(null);
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+
     return (
         <div>
             <button onClick={handleGoogleSignIn}>google login</button>
+            <button onClick={handleSignOut}>sign out</button>
+            { user &&
+             <div>
+                <h3>User: {user.displayName}</h3>
+                <p>email :{user.email}</p>
+                <img src={user.photoURL} alt="" />
+            </div>
+            }
         </div>
     );
 };
